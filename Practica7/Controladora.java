5e5f26f4350ad5ad0611d6286adebf53fb1b5336
Berenice <Berenice@DESKTOP-J4VPCEU>
2023-11-22 23:12:51 -0600

    Se agrega .bin y .obj

--- Practica7/Controladora.java
+++ Practica7/Controladora.java
@@ -4,26 +4,12 @@ import java.io.*;
 public class Controladora {
 	public static void main(String [] args) {
 		
-		try{
-			DataOutputStream escritor = new	DataOutputStream(new FileOutputStream("hola.bin"));
-
-			escritor.writeInt(33465);
-			escritor.writeChar('a');
-			escritor.writeChar('z');
-			escritor.writeShort(10);
-
-			escritor.close();
-
-			DataInputStream	lector = new DataInputStream(new FileInputStream("hola.bin"));
-
-			System.out.println(lector.readChar());
-			System.out.println(lector.readShort());
-			System.out.println(lector.readInt());
-			System.out.println(lector.readChar());
-
-			escritor.close();
-
-		}catch(IOException e){
-		}
+		ObjectInputStream	lector	=	new	ObjectInputStream
+				(new	FileInputStream(“web.obj”));
+	PrintWriter	escritor	=	new	PrintWriter(“main.html”);
+	WebMaker	pagina =	(WebMaker)(lector.readObject());
+	escritor.print(pagina.printPage());
+	lector.close();
+	escritor.close();
 	}
 }
