f021129994fd1bb5f64714ae2799e084263c0a15
Berenice <Berenice@DESKTOP-J4VPCEU>
2023-11-22 22:49:45 -0600

    Codigo nuevo

--- Practica7/Controladora.java
+++ Practica7/Controladora.java
@@ -3,26 +3,17 @@ import java.io.*;
 
 public class Controladora {
 	public static void main(String [] args) {
-		WebMaker generador = new WebMaker();
-
-		try{
-			PrintWriter escritor = new PrintWriter("index.txt");
-			Scanner lector = new Scanner(new FileReader("form.txt"));
-		
-			escritor.println(generador.imprimirEncabezado());
-
-			escritor.println(generador.imprimirTexto("Un gatito!"));
-			escritor.println(generador.insertarImagen("gato.jpg"));
-		
-			while(lector.hasNext())	{
-				escritor.println(lector.nextLine());
-			}
-			escritor.println(generador.imprimirCierre());
-			
-			lector.close();
-			escritor.close();
-		}catch(FileNotFoundException e){
-
-		}
+		DataOutputStream escritor = new	DataOutputStream(new FileOutputStream(“hola.bin”));
+		escritor.writeInt(33465);
+		escritor.writeChar(‘a’);
+		escritor.writeChar(‘z’);
+		escritor.writeShort(10);
+		escritor.close()
+		DataInputStream	lector = new DataInputStream(new FileInputStream(“hola.bin”));
+		System.out.println(lector.readInt());
+		System.out.println(lector.readChar());
+		System.out.println(lector.readChar());
+		System.out.println(lector.readShort());
+		escritor.close();
 	}
 }
