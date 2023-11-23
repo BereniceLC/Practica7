e1c3c6af0a0d2ba3c7f1bfb9efb488d41d8ec13c
Berenice <Berenice@DESKTOP-J4VPCEU>
2023-11-22 19:17:42 -0600

    Creacion y declaracion

--- Practica7/Controladora.java
+++ Practica7/Controladora.java
@@ -1,4 +1,12 @@
+java.util.Scanner;
+java.io.*;
 
 public class Controladora {
-
+	public static void main(String[]args){
+		WebMaker generador = new WebMaker();
+		PrintWriter escritor = new PrintWriter("index.txt");
+		Scanner lector = new Scanner(new FileWriter("form.txt"));
+		
+		escritor.println(generador.imprimirEncabezado());
+	}
 }
