f886858dc179df4e867ed9e6d4c6db5ffaf2111a
Berenice <Berenice@DESKTOP-J4VPCEU>
2023-11-22 21:44:33 -0600

    Agregar try catch para posibles excepciones

--- Practica7/Controladora.java
+++ Practica7/Controladora.java
@@ -5,19 +5,25 @@ public class Controladora {
 	public static void main(String[]args){
 		
 		WebMaker generador = new WebMaker();
-		PrintWriter escritor = new PrintWriter("index.txt");
-		Scanner lector = new Scanner(new FileReader("form.txt"));
 		
-		escritor.println(generador.imprimirEncabezado())
+		try {
+			PrintWriter escritor = new PrintWriter("index.txt");
+			Scanner lector = new Scanner(new FileReader("form.txt"));
 		
-		escritor.println(generador.imprimirTexto("Un gatito!"));
-		escritor.println(generador.insertarImagen("gato.jpg"));
+			escritor.println(generador.imprimirEncabezado())
 		
-		while(lector.hasNext())	{
-			escritor.println(lector.nextLine());
+			escritor.println(generador.imprimirTexto("Un gatito!"));
+			escritor.println(generador.insertarImagen("gato.jpg"));
+		
+			while(lector.hasNext())	{
+				escritor.println(lector.nextLine());
+			}
+			escritor.println(generador.imprimirCierre());
+			lector.close();
+			escritor.close();
+			
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
 		}
-		escritor.println(generador.imprimirCierre());
-		lector.close();
-		escritor.close();
 	}
 }
