29bddf1003bc587e2e3fd1c98905dda655a0a0d0
Berenice <Berenice@DESKTOP-J4VPCEU>
2023-11-22 23:16:18 -0600

    Ultimo cambio

--- Practica7/Controladora.java
+++ Practica7/Controladora.java
@@ -3,13 +3,13 @@ import java.io.*;
 
 public class Controladora {
 	public static void main(String [] args) {
-		
-		ObjectInputStream	lector	=	new	ObjectInputStream
-				(new	FileInputStream(“web.obj”));
-	PrintWriter	escritor	=	new	PrintWriter(“main.html”);
-	WebMaker	pagina =	(WebMaker)(lector.readObject());
-	escritor.print(pagina.printPage());
-	lector.close();
-	escritor.close();
+		try{
+			ObjectInputStream lector = new ObjectInputStream(new FileInputStream("web.obj"));
+			PrintWriter escritor = new PrintWriter("main.html");
+			WebMaker pagina = (WebMaker)(lector.readObject());
+			escritor.print(pagina.printPage());
+			lector.close();
+			escritor.close();
+			}catch(Exception e){}
 	}
 }
