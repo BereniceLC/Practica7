b25fbd2ed509d7000122480131e1a051a9e72216
Berenice <Berenice@DESKTOP-J4VPCEU>
2023-11-22 22:56:47 -0600

    Cambios y el try catch

--- Practica7/Controladora.java
+++ Practica7/Controladora.java
@@ -3,17 +3,27 @@ import java.io.*;
 
 public class Controladora {
 	public static void main(String [] args) {
-		DataOutputStream escritor = new	DataOutputStream(new FileOutputStream(“hola.bin”));
-		escritor.writeInt(33465);
-		escritor.writeChar(‘a’);
-		escritor.writeChar(‘z’);
-		escritor.writeShort(10);
-		escritor.close()
-		DataInputStream	lector = new DataInputStream(new FileInputStream(“hola.bin”));
-		System.out.println(lector.readInt());
-		System.out.println(lector.readChar());
-		System.out.println(lector.readChar());
-		System.out.println(lector.readShort());
-		escritor.close();
+		
+		try{
+			DataOutputStream escritor = new	DataOutputStream(new FileOutputStream("hola.bin"));
+
+			escritor.writeInt(33465);
+			escritor.writeChar('a');
+			escritor.writeChar('z');
+			escritor.writeShort(10);
+
+			escritor.close();
+
+			DataInputStream	lector = new DataInputStream(new FileInputStream("hola.bin"));
+
+			System.out.println(lector.readInt());
+			System.out.println(lector.readChar());
+			System.out.println(lector.readChar());
+			System.out.println(lector.readShort());
+
+			escritor.close();
+
+		}catch(IOException e){
+		}
 	}
 }
